d9bb9bef6dd77f96d41246e9d3fe5d5b15634298
noe779 <anadelpilarmazapingo@gmail.com>
2024-07-25 14:57:08 -0500

    Add files via upload

---
+++ it's for you/main.js
@@ -0,0 +1,10 @@
+onload = () =>{
+    document.body.classList.remove("container");
+    
+
+};
+
+function playAudioAndRedirect() {
+        // Esta función ahora solo se encarga de la redirección
+        window.location.href = "flower.html";
+      }
